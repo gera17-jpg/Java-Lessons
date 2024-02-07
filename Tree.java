6930753dafd9255579cce1c7b63ce7c22a7e9fc5
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-02-07 13:56:27 +0800

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,55 @@
+/*Planting a Tree
+Introduce yourself to users and plant a tree for them!
+
+Tasks
+7/7 complete
+
+We’re starting with a blank Java file named Tree.java.
+
+Define a class that follows the Java naming conventions.
+
+
+This code produces an error because our program needs a main() method.
+
+Define this method inside the curly braces of the Tree class.
+
+
+
+Write a comment in main() that describes the task it will perform.
+
+We’re going to introduce ourselves and print a tree to the screen.
+
+You can use the single-line or multi-line syntax for your comment.
+
+
+
+Below your comment, print a message introducing yourself to the user.
+
+Try something like “Hey there, I’m Ariel and I’m learning to code in Java!”
+
+When you run the code, you should see this message printed to the screen.
+
+
+
+After introducing yourself, use another print statement to output the project goal!
+
+Something like “I’m going to plant a tree today!” or “Ready to get my hands dirty!”
+
+
+See how the second statement begins on a new line? We’ll use multiple print statements to plant our tree.*/
+
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+    //This program will Introduce yourself to users and plant a tree for them!
+
+   System.out.print("Hello my name is Gem.");
+   System.out.println("I'm going to plant a tree for you!Yey!!(*note the sarcasm c-_-)");
+   System.out.println("   *  ");
+   System.out.println("  *** ");
+   System.out.println(" *****");
+   System.out.println("********");
+   System.out.println("   *");
+   System.out.println("   *");
+  }
+}
