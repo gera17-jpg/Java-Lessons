e20dfc721839c984f02e13696511f267a8e93ce1
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-02-07 11:11:06 +0800

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,17 @@
+public class Day1{
+  public static void main(String[] args)//methods or classes don't need semicolon{
+    //I learned how to make comments
+    /* For
+      multi
+      lines*/
+    /** For API documentation*/
+    System.out.print("Java needs to be double qouted");
+    System.out.println("out is short term for outline and prinln is for new line");
+  }
+}
+public class Compiling{
+  public static void main(String[] args){
+    System.out.print("I also learned how to compile");
+  }
+}
+  
