b17642e6341708d00c17088b0b2e287bce0479e1
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-03-01 16:35:53 +0800

    Create CoffeeForLoop.java
    -for loop

---
+++ CoffeeForLoop.java
@@ -0,0 +1,9 @@
+class CoffeeForLoop {
+  
+  public static void main(String[] args) {
+    
+    for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);   
+    }    
+  }  
+}
