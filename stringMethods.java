7416a5af3357478468318c8c7ca91e487533e065
Gemma Yubos <142143749+gera17-jpg@users.noreply.github.com>
2024-03-03 13:14:28 +0800

    Create stringMethods.java

---
+++ stringMethods.java
@@ -0,0 +1,25 @@
+class stringMethods{
+  public sattic void main(String[] args){
+    String firstname = "Vaine";
+    String lastNamee = "Lweis";
+    String password = "dkfjdkjsi2090930jf";
+  //length method
+    System.out.println(password.length);
+  //concat() method
+     System.out.println(firstName.concat(lastName);
+  //equals()
+     System.out.println(password.equals("djdksjsfojfos);
+  //equalsIgnoreCase()
+     System.out.println(firstName.equalsIgnoreCase(vaine));
+  //compareTo()
+     System.out.println(firstName.compareTo(password);
+  //indexOf()
+     System.out.println(lastName.indexOf("e");
+  //charAt()
+    System.out.println(password.charAt(5));
+  //substring()
+     System.out.println(password.substring(3,6));
+  //toUpperCase() / toLowerCase()
+     System.out.println(password.toUpperCase());
+  }
+}
